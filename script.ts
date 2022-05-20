c7966140ca3d2218c4592c6c5ec2406afb5308fa
AdalbertCosta <contato@adalbertocosta.com>
2022-05-20 07:24:42 -0300

    Criando sistema de estacionamento

--- script.ts
+++ script.ts
@@ -1,13 +1,69 @@
-(function () {
-  const $ = (query: string): HTMLInputElement | null => document.querySelector(query);
+interface Veiculo {
+  nome: string
+  placa: string
+  entrada: Date
+}
 
-  $("#cadastrar")?.addEventListener("click", () => {
-    const nome = $("#nome")?.value;
-    const placa = $("#placa")?.value;
+;(function () {
+  const $ = (query: string): HTMLInputElement | null =>
+    document.querySelector(query);
 
-    if(!nome || !placa) {
-      alert("Os campos nome e placa são obirgatórios");
-      return;
+  function patio() {
+    function ler(): Veiculo [] {
+      return localStorage.patio ? JSON.parse(localStorage.patio) : [];
     }
-  })
-})();
+
+    function salvar(veiculos: Veiculo[]) {
+      localStorage.setItem("patio",JSON.stringify(veiculos));
+    }
+
+    function adicionar(veiculo: Veiculo, salva?: boolean) {
+      const row = document.createElement('tr');
+
+      row.innerHTML = `
+        
+        <td>${veiculo.entrada}</td>
+        <td>${veiculo.placa}</td>
+        <td>${veiculo.entrada}</td>
+        <td>
+        <button class="delete" data-placa="${veiculo.placa}">X</button>
+        </td>
+        `
+
+      $("#patio")?.appendChild(row);
+
+      if(salva) salvar([...ler(), veiculo]);
+    }
+
+    function remover() {}
+
+    
+
+    function render() {
+      $("#patio")!.innerHTML = "";
+      const patio = ler();
+
+      if(patio.length) {
+        patio.forEach((veiculo) => adicionar(veiculo));
+
+      }
+    }
+
+    return { ler, adicionar, remover, salvar, render }
+  }
+
+    patio().render();
+
+
+  $('#cadastrar')?.addEventListener('click', () => {
+    const nome = $('#nome')?.value;
+    const placa = $('#placa')?.value;
+
+    if (!nome || !placa) {
+      alert('Os campos nome e placa são obirgatórios')
+      return
+    }
+
+    patio().adicionar({ nome, placa, entrada: new Date() }, true)
+  });
+})()
